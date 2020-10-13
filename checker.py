20b39b675d08afe414a9d7def04e0649acd18ab3
ddhira123 <dhana_dhira@apps.ipb.ac.id>
2020-10-13 21:40:05 +0700

    Config checker etc.

---
+++ checker.py
@@ -0,0 +1,33 @@
+import os
+from alphabet_detector import AlphabetDetector
+
+def check_double(file):
+    words = []
+    for word in file.readlines():
+        if word in words:
+            return False
+        else:
+            words.append(word)
+    return True
+
+def check_lower(file):
+    ad = AlphabetDetector()
+    for word in file.readlines():
+        if ad.is_latin(word) and word.islower() == False:
+            return False
+    return True
+
+def runner():
+    for filename in os.listdir('list'):
+        file = open("./list/" + filename, encoding='utf-8')
+        if check_double(file) == False:
+            file.close()
+            return "Duplicate records"
+        elif check_lower(file) == False:
+            file.close()
+            return "Uppercase!"
+        file.close()
+        return "PASS"
+
+    
+
---
+++ test_unittest.py
@@ -0,0 +1,4 @@
+import checker
+
+def test_check_double():
+    assert checker.runner() == "PASS"
